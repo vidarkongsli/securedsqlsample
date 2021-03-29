c08549461d671f614fb198fb06777cfdf41b55a7
Vidar Kongsli <vidar@kongs.li>
2021-03-29 09:00:05 +0200

    Add command line options

--- src/SecuredSqlSample.Web/Program.cs
+++ src/SecuredSqlSample.Web/Program.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,15 +11,35 @@ namespace SecuredSqlSample.Web
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
+
+            var rootCommand = new RootCommand{
+                new Option<bool>("--migrate", "Whether to run database migrations"),
+                new Option<bool>("--thenExit", "Whether to exit after migrations (instead of starting web app)")
+            };
+
+            rootCommand.Handler = CommandHandler.Create<bool, bool>(async (migrate, thenExit) =>
             {
-                var profileDbContext = scope.ServiceProvider.GetService<ProfileDbContext>();
-                await profileDbContext.Database.MigrateAsync();
-            }
-            await host.RunAsync();
+                if (migrate)
+                {
+                    using (var scope = host.Services.CreateScope())
+                    {
+                        var services = scope.ServiceProvider;
+                        var logger = services.GetRequiredService<ILogger<Program>>();
+                        logger.LogInformation("Running migrations");
+                        var profileDbContext = services
+                            .GetService<ProfileDbContext>();
+                        await profileDbContext.Database.MigrateAsync();
+                    }
+                }
+                if (!thenExit)
+                {
+                    await host.RunAsync();
+                }
+            });
+            return await rootCommand.InvokeAsync(args);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
